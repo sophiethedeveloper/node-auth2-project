55b84c17f7c3571bdbcab7020b2be296c401a031
sofia Luna <Sofia21luna@gmail.com>
2020-11-10 16:30:31 -0600

    login works

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,10 @@
 const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 
 const Users = require("../users/users-models");
 const { isValid } = require("../users/users-service");
-// const { jwtSecret } = require("./secrets.js")
+const { jwtSecret } = require("./secrets.js");
 
 router.post("/register", (req, res) => {
   const credentials = req.body;
@@ -28,7 +28,44 @@ router.post("/register", (req, res) => {
       message: "please provide username and password",
     });
   }
-  console.log("body", credentials);
 });
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (isValid(req.body)) {
+    Users.findBy({ username })
+      .then(([user]) => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = makeToken(user);
+
+          res.status(200).json({ message: "welcome to the API", token });
+        } else {
+          res.status(401).json({ message: "Invalid credentials" });
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ message: error.message });
+      });
+  } else {
+    res.status(400).json({
+      message:
+        "please provide username and password and the password should be alphanumeric",
+    });
+  }
+});
+
+function makeToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role,
+  };
+
+  const options = {
+    expiresIn: "45 seconds",
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
